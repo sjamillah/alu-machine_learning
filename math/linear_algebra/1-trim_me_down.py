3cc9389dc4e7cd3c0ab33bf3477685a2c67c85ac
sjamillah <j.ssozi@alustudent.com>
2024-09-12 18:15:52 +0200

    debugged the 1-trim question

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
-for (i, j) in the_middle:
-    print(matrix[3][4])
+the_middle = [[row[2], row[3]] for row in matrix]
 print("The middle columns of the matrix are: {}".format(the_middle))
