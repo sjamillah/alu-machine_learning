08b25f3fbc6671887c4f064aee192fb1bfcad150
sjamillah <j.ssozi@alustudent.com>
2024-09-14 02:50:34 +0200

    solved the error

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -7,29 +7,28 @@ Matrix addition
 
 def add_matrices(mat1, mat2):
     # Check if the matrices have the same shape
-    if not isinstance(mat1, list) or not isinstance(mat2, list):
-        return None
 
-
-def check_shape(m1, m2):
-    if isinstance(m1, list) and isinstance(m2, list):
-        if len(m1) != len(m2):
-            return False
-        for sub1, sub2 in zip(m1, m2):
-            if not check_shape(sub1, sub2):
+    def check_shape(m1, m2):
+        if isinstance(m1, list) and isinstance(m2, list):
+            if len(m1) != len(m2):
                 return False
+            for sub1, sub2 in zip(m1, m2):
+                if not check_shape(sub1, sub2):
+                    return False
             return True
         return True
 
+# Check the shapes of mat1 and mat2
     if not check_shape(mat1, mat2):
         return None
 
 
-def add_elementwise(m1, m2):
     # Function to add matrices element-wise
-    if isinstance(m1, list) and isinstance(m2, list):
-        return [add_elementwise(sub1, sub2) for sub1, sub2 in zip(m1, m2)]
-    else:
-        return m1 + m2
+    def add_elementwise(m1, m2):
+        # Function to add matrices element-wise
+        if isinstance(m1, list) and isinstance(m2, list):
+            return [add_elementwise(sub1, sub2) for sub1, sub2 in zip(m1, m2)]
+        else:
+            return m1 + m2
     
-return add_elementwise(mat1, mat2)
+    return add_elementwise(mat1, mat2)
