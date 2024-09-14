12cb91d6ac53faf1a03a5a58a3e4c6d3365b7414
sjamillah <j.ssozi@alustudent.com>
2024-09-14 02:26:16 +0200

    advanced tasks

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,28 +1,95 @@
 #!/usr/bin/env python3
-
+import numpy as np
 def cat_matrices(mat1, mat2, axis=0):
-    if axis == 0:
-        if len(mat1[0]) != len(mat2[0]):
-            return None
-        return mat1 + mat2
-    elif axis == 1:
-        if len(mat1) != len(mat2):
-            return None
-        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
-    elif axis == 2:
-        if not isinstance(mat1[0], list) or not isinstance(mat2[0], list):
-            return None
-        if len(mat1) != len(mat2):
-            return None
-        if len(mat1[0]) != len(mat2[0]):
-            return None
-        return [x + y for x, y in zip(mat1, mat2)]
-    else:
+    try:
+        np_mat1 = np.array(mat1)
+        np_mat2 = np.array(mat2)
+        return np.concatenate((np_mat1, np_mat2), axis=axis).tolist()
+    except:
         return None
 
-
 if __name__ == "__main__":
+    import time
+
+    mat1 = [1, 2, 3]
+    mat2 = [4, 5, 6]
+    np_mat1 = np.array(mat1)
+    np_mat2 = np.array(mat2)
+
+    t0 = time.time()
+    m = cat_matrices(mat1, mat2)
+    t1 = time.time()
+    print(t1 - t0)
+    print(m)
+    t0 = time.time()
+    np.concatenate((np_mat1, np_mat2))
+    t1 = time.time()
+    print(t1 - t0, "\n")
+
     mat1 = [[1, 2], [3, 4]]
     mat2 = [[5, 6], [7, 8]]
-    print(cat_matrices(mat1, mat2))
-    print(cat_matrices(mat1, mat2, axis=1))
+    np_mat1 = np.array(mat1)
+    np_mat2 = np.array(mat2)
+
+    t0 = time.time()
+    m = cat_matrices(mat1, mat2)
+    t1 = time.time()
+    print(t1 - t0)
+    print(m)
+    t0 = time.time()
+    np.concatenate((np_mat1, np_mat2))
+    t1 = time.time()
+    print(t1 - t0, "\n")
+
+    t0 = time.time()
+    m = cat_matrices(mat1, mat2, axis=1)
+    t1 = time.time()
+    print(t1 - t0)
+    print(m)
+    t0 = time.time()
+    np.concatenate((np_mat1, np_mat2), axis=1)
+    t1 = time.time()
+    print(t1 - t0, "\n")
+
+    mat3 = [[[[1, 2, 3, 4], [5, 6, 7, 8]],
+             [[9, 10, 11, 12], [13, 14, 15, 16]],
+             [[17, 18, 19, 20], [21, 22, 23, 24]]],
+            [[[25, 26, 27, 28], [29, 30, 31, 32]],
+             [[33, 34, 35, 36], [37, 38, 39, 40]],
+             [[41, 42, 43, 44], [45, 46, 47, 48]]]]
+    mat4 = [[[[11, 12, 13, 14], [15, 16, 17, 18]],
+             [[19, 110, 111, 112], [113, 114, 115, 116]],
+             [[117, 118, 119, 120], [121, 122, 123, 124]]],
+            [[[125, 126, 127, 128], [129, 130, 131, 132]],
+             [[133, 134, 135, 136], [137, 138, 139, 140]],
+             [[141, 142, 143, 144], [145, 146, 147, 148]]]]
+    mat5 = [[[[11, 12, 13, 14], [15, 16, 17, 18]],
+             [[117, 118, 119, 120], [121, 122, 123, 124]]],
+            [[[125, 126, 127, 128], [129, 130, 131, 132]],
+             [[141, 142, 143, 144], [145, 146, 147, 148]]]]
+    np_mat3 = np.array(mat3)
+    np_mat4 = np.array(mat4)
+    np_mat5 = np.array(mat5)
+
+    t0 = time.time()
+    m = cat_matrices(mat3, mat4, axis=3)
+    t1 = time.time()
+    print(t1 - t0)
+    print(m)
+    t0 = time.time()
+    np.concatenate((np_mat3, np_mat4), axis=3)
+    t1 = time.time()
+    print(t1 - t0, "\n")
+
+    t0 = time.time()
+    m = cat_matrices(mat3, mat5, axis=1)
+    t1 = time.time()
+    print(t1 - t0)
+    print(m)
+    t0 = time.time()
+    np.concatenate((np_mat3, np_mat5), axis=1)
+    t1 = time.time()
+    print(t1 - t0, "\n")
+
+    m = cat_matrices(mat2, mat5)
+    print(m)
