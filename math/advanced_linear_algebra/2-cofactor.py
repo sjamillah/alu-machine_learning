c65a656267f1b1c2f013ca352dece16fbb10c46b
sjamillah <j.ssozi@alustudent.com>
2024-09-21 00:55:18 +0200

    cofactor matrix of a matrix

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Calculates the minor matrix of a matrix"""
+"""Function that calculates the cofactor matrix of a matrix"""
 
 
 def determinant(matrix):
-    """Function that calculates the determinant of a matrix"""
+    """Calculates the determinant of a matrix"""
     if len(matrix) == 2:
         return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]))
     det = []
@@ -18,8 +18,8 @@ def determinant(matrix):
     return sum(det)
 
 
-def minor(matrix):
-    """Function that calculates the minor matrix of a matrix"""
+def cofactor(matrix):
+    """Calculates the cofactor matrix of a matrix"""
     if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
     for i in matrix:
@@ -31,14 +31,21 @@ def minor(matrix):
     if len(matrix) == 1 and len(matrix) == 1:
         return [[1]]
     if len(matrix) == 2:
-        minor = [i[::-1] for i in matrix]
-        return minor[::-1]
-    minor = [[j for j in matrix[i]] for i in range(len(matrix))]
+        cofactor = [i[::-1] for i in matrix]
+        cofactor = cofactor[::-1]
+        cofactor = [[cofactor[i][j] if (i + j) % 2 == 0 else -cofactor[i][j]
+                     for j in range(len(cofactor[i]))]
+                    for i in range(len(cofactor))]
+        return cofactor
+    cofactor = [[j for j in matrix[i]] for i in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             mini = [[j for j in matrix[i]] for i in range(len(matrix))]
             mini = mini[:i] + mini[i + 1:]
             for k in range(len(mini)):
                 mini[k].pop(j)
-            minor[i][j] = determinant(mini)
-    return minor
+            if (i + j) % 2 == 0:
+                cofactor[i][j] = determinant(mini)
+            if (i + j) % 2 == 1:
+                cofactor[i][j] = -1 * determinant(mini)
+    return cofactor
