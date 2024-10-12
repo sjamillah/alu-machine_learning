8ed224e40711893e872502c6238c41d4fb5d27e3
sjamillah <j.ssozi@alustudent.com>
2024-10-12 16:39:33 +0200

    created a pdf instance method to calculate the pdf

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -12,6 +12,9 @@ class Binomial:
     instance attributes:
         n [int]: the number of Bernoilli trials
         p [float]: the probability of a success
+
+    instance methods:
+        def pmf(self, k): calculates PMF for given number of successes
     """
 
     def __init__(self, data=None, n=1, p=0.5):
@@ -59,3 +62,35 @@ class Binomial:
                 p = float(mean / n)
                 self.n = n
                 self.p = p
+
+    def pmf(self, k):
+        """
+        calculates the value of the PMF for a given number of successes
+
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the PMF value for k
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        p = self.p
+        n = self.n
+        q = (1 - p)
+        n_factorial = 1
+        for i in range(n):
+            n_factorial *= (i + 1)
+        k_factorial = 1
+        for i in range(k):
+            k_factorial *= (i + 1)
+        nk_factorial = 1
+        for i in range(n - k):
+            nk_factorial *= (i + 1)
+        binomial_co = n_factorial / (k_factorial * nk_factorial)
+        pmf = binomial_co * (p ** k) * (q ** (n - k))
+        return pmf
