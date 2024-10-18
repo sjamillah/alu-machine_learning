59654333d8df769d4f5d3e07f84e3d4b15673c9a
sjamillah <j.ssozi@alustudent.com>
2024-10-19 00:49:37 +0200

    likelihood to get severe side effects from a drug

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -37,7 +37,7 @@ def likelihood(x, n, P):
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if np.any((P < 0) | (P > 1)):
+    if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range[0, 1]")
     numerator = np.math.factorial(n)
     denominator = np.math.factorial(x) * np.math.factorial(n - x)
