ebac6548c7cf370022b083c3ea5d29d1db25f566
sjamillah <j.ssozi@alustudent.com>
2024-10-25 22:48:51 +0200

    convolution on grayscaled images

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -25,18 +25,17 @@ def convolve_grayscale_valid(images, kernel):
     m, h, w = images.shape[0], images.shape[1], images.shape[2]
     kh, kw = kernel.shape[0], kernel.shape[1]
     if kh % 2 == 1:
-        padding_height = (kh - 1) // 2
+        ph = (kh - 1) // 2
     else:
-        padding_height = kh // 2
+        ph = kh // 2
     if kw % 2 == 1:
-        padding_width = (kw - 1) // 2
+        pw = (kw - 1) // 2
     else:
-        padding_width = kw // 2
+        pw = kw // 2
 
     convolved = np.zeros((m, h, w))  # initialize output array
     # pad the images with zeros
-    npad = ((0, 0), (padding_height, padding_height),
-            (padding_width, padding_width))
+    npad = ((0, 0), (ph, ph), (pw, pw))
     imagesp = np.pad(images, pad_width=npad,
                      mode='constant', constant_values=0)
     # the convolution operation
