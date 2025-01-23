8262361e7f54a9f782ade252d8774b50abcd2e4c
sjamillah <j.ssozi@alustudent.com>
2025-01-23 22:29:01 +0200

    definition of a single neuron

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,9 +12,6 @@ class Neuron:
     Defines a single neuron performing binary classification.
     Attributes:
         nx(int): The number of input features to the neuron.
-        __W(numpy.ndarray): The weight vector for the neuron
-        __b(float): The bias for the neuron
-        __A(float): The activated output of the neuron
 
     Raises:
         TypeError: If nx is not an integer.
@@ -37,26 +34,5 @@ class Neuron:
 
         # Initialize weight vector with random normal distribution
         self.__W = np.random.normal(size=(1, nx))
-        self.__b = 0  # Private bias for the neuron
-        self.__A = 0  # Private activated output of the neuron(prediction)
-
-        @property
-        def W(self):
-            """
-            Getter for the weight vector
-            """
-            return self.__W
-        
-        @property
-        def b(self):
-            """
-            Getter for the bias
-            """
-            return self.__b
-        
-        @property
-        def A(self):
-            """
-            Getter for the activated output
-            """
-            return self.__A
+        self.__b = 0  # The bias for the neuron
+        self.__A = 0  # The activated output of the neuron(prediction)
