5699ff62be873d70e61d0af3f269f191387c181f
sjamillah <j.ssozi@alustudent.com>
2025-01-31 19:54:23 +0200

    task 0 - create placeholder

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Function returns two placeholders x and y for the neural network 
+"""
+
+
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """
+    Returns two placeholders x and y
+    
+    Args:
+    nx: the number of feature columns in the data
+    classes: the number of classes in the classifier
+
+    Returns:
+    x: placeholder for the input data
+    y: placeholder for the one-hot labels for the input data
+    """
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+    return x, y
