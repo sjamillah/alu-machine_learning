cf7d4fede99c25fd24972eb6e8e7e026c69c9575
sjamillah <j.ssozi@alustudent.com>
2025-02-06 22:07:40 +0200

    calculates the cost with l2 regularizer

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -4,6 +4,9 @@ Function calculates the cost of a neural network with L2 regularization
 """
 
 
+import numpy as np
+
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     Calculates the cost of a neural network with L2 regularization
