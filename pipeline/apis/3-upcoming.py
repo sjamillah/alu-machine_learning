e9f155c6a3352c003c2084260895638dd1c31417
sjamillah <j.ssozi@alustudent.com>
2025-03-02 12:57:00 +0200

    machine learning api

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,64 +1,31 @@
 #!/usr/bin/env python3
-"""
-    script that displays the upcoming launch
-"""
-
-
+"""Pipeline Api"""
 import requests
-from datetime import datetime, timezone, timedelta
+from datetime import datetime
 
 
-def get_upcoming_launch():
-    """
-    Displays the upcoming launch
-    """
+if __name__ == '__main__':
+    """pipeline api"""
     url = "https://api.spacexdata.com/v4/launches/upcoming"
-    response = requests.get(url)
-    launches = response.json()
+    r = requests.get(url)
+    recent = 0
 
-    # Sort launches by date_unix to find the soonest launch
-    launches.sort(key=lambda x: x["date_unix"])
-    upcoming_launch = launches[0]
+    for dic in r.json():
+        new = int(dic["date_unix"])
+        if recent == 0 or new < recent:
+            recent = new
+            launch_name = dic["name"]
+            date = dic["date_local"]
+            rocket_number = dic["rocket"]
+            launch_number = dic["launchpad"]
 
-    launch_name = upcoming_launch["name"]
-    date_unix = upcoming_launch["date_unix"]
-    rocket_id = upcoming_launch["rocket"]
-    launchpad_id = upcoming_launch["launchpad"]
+    rurl = "https://api.spacexdata.com/v4/rockets/" + rocket_number
+    rocket_name = requests.get(rurl).json()["name"]
+    lurl = "https://api.spacexdata.com/v4/launchpads/" + launch_number
+    launchpad = requests.get(lurl)
+    launchpad_name = launchpad.json()["name"]
+    launchpad_local = launchpad.json()["locality"]
+    string = "{} ({}) {} - {} ({})".format(launch_name, date, rocket_name,
+                                           launchpad_name, launchpad_local)
 
-    # Convert the date to the desired local time (UTC-4)
-    launch_date_utc = datetime.fromtimestamp(date_unix, tz=timezone.utc)
-    launch_date_local = launch_date_utc.astimezone(timezone(
-        timedelta(hours=-4))
-    )
-    launch_date_str = launch_date_local.strftime('%Y-%m-%dT%H:%M:%S%z')
-    launch_date_str = "{}:{}".format(
-        launch_date_str[:-2], launch_date_str[-2:]
-    )
-
-    # Get rocket name
-    rocket_url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
-    rocket_response = requests.get(rocket_url)
-    rocket_name = rocket_response.json()["name"]
-
-    # Get launchpad details
-    launchpad_url = "https://api.spacexdata.com/v4/launchpads/{}".format(
-        launchpad_id
-    )
-    launchpad_response = requests.get(launchpad_url)
-    launchpad_data = launchpad_response.json()
-    launchpad_name = launchpad_data["name"]
-    launchpad_locality = launchpad_data["locality"]
-
-    # Format the output
-    formatted_output = "{} ({}) {} - {} ({})".format(
-        launch_name,
-        launch_date_str,
-        rocket_name,
-        launchpad_name,
-        launchpad_locality,
-    )
-    return formatted_output
-
-
-if __name__ == "__main__":
-    print(get_upcoming_launch())
+    print(string)
