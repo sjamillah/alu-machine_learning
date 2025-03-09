0ca0491bcef6e5da97ae9c3e6b73fb1fba690ca8
sjamillah <j.ssozi@alustudent.com>
2025-03-09 23:27:11 +0200

    databases

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -2,21 +2,21 @@
 -- for a user and update the average_score column in the users table.
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
+CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id_param INT
 )
 BEGIN
-DECLARE user_avg_score FLOAT
-
--- Calculate average score for the user
-SELECT AVG(score) INTO user_avg_score
-FROM corrections
-WHERE user_id = user_id_param
-
--- Update the average_score column in the users table
-UPDATE users
-SET average_score = user_avg_score
-WHERE id = user_id_param
+    DECLARE user_avg_score FLOAT;
+    
+    -- Calculate average score for the user
+    SELECT AVG(score) INTO user_avg_score
+    FROM corrections
+    WHERE user_id = user_id_param;
+    
+    -- Update the average_score column in the users table
+    UPDATE users
+    SET average_score = user_avg_score
+    WHERE id = user_id_param;
 END //
 
-DELIMITER
+DELIMITER ;
--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -3,9 +3,9 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 
 BEGIN
-IF b = 0 THEN
-RETURN 0
-ELSE
-RETURN a / b
-END IF
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
 END $$
